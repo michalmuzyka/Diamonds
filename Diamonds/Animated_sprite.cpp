d40221d75fc5157762ec93e4fcbf69aa4d08ebee
muzyczek <michal.muzyka@o2.pl>
2018-08-13 22:26:48 +0200

    updated animations

--- Diamonds/Animated_sprite.cpp
+++ Diamonds/Animated_sprite.cpp
@@ -1,16 +1,67 @@
 #include "stdafx.h"
 #include "Animated_sprite.h"
+#include <SFML/Graphics/RenderWindow.hpp>
 
 namespace di
 {
 
-    Animated_sprite::Animated_sprite()
+    Animated_sprite::Animated_sprite(const double& step_time=sf::seconds(0.2).asMicroseconds(), const bool& paused=false, const bool& looped=true)
+        :animation(nullptr), texture(nullptr), frame_number(0), looped(looped), paused(paused), step_time(step_time)
     {
+        
     }
 
-
-    Animated_sprite::~Animated_sprite()
-    {
+    void Animated_sprite::update(const double& delta_time) {
+        
     }
 
+    void Animated_sprite::play() {
+        paused = false;
+    }
+
+    bool Animated_sprite::is_looped() const {
+        return looped;
+    }
+
+    bool Animated_sprite::is_paused() const {
+        return paused;
+    }
+
+    void Animated_sprite::pause() {
+        paused = true;
+    }
+
+    std::shared_ptr<di::Animation> Animated_sprite::get_animation() const {
+        return animation;
+    }
+
+    double Animated_sprite::get_step_time() const {
+        return step_time;
+    }
+
+    void Animated_sprite::set_animation(std::shared_ptr<Animation> animation) {
+        this->animation = animation;
+        texture = animation->get_sheet();
+        set_frame(0);
+    }
+
+    void Animated_sprite::set_frame(const std::size_t& n) {
+        sprite.setTextureRect(animation->get_frame(n));
+    }
+
+    void Animated_sprite::set_loop(const bool& loop) {
+        looped = loop;
+    }
+
+    void Animated_sprite::set_step_time(const double& time) {
+        step_time = time;
+    }
+
+    void Animated_sprite::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+        if (animation&&texture) {
+            states.transform *= getTransform();
+            states.texture = *texture;
+            target.draw(sprite);
+        }
+    }
 }
--- Diamonds/Animated_sprite.h
+++ Diamonds/Animated_sprite.h
@@ -11,13 +11,34 @@ namespace di
     class Animated_sprite :public sf::Drawable, public sf::Transformable
     {
     public:
-        Animated_sprite();
-        ~Animated_sprite();
+        explicit Animated_sprite(const double &step_time, const bool &paused, const bool &looped);
+
+        void update(const double &delta_time);
+
+        void set_animation(std::shared_ptr<Animation> animation);
+        void set_frame(const std::size_t &n);
+        void set_loop(const bool &loop);
+        void set_step_time(const double &time);
+        void pause();
+        void play();
+
+        std::shared_ptr<di::Animation> get_animation() const;
+        double get_step_time() const;
+        bool is_paused() const;
+        bool is_looped() const;
+
     private:
-        std::shared_ptr<di::Animation> animation;
-        std::shared_ptr<sf::Texture> texture;
+        const std::shared_ptr<di::Animation> animation;
+        const std::shared_ptr<sf::Texture> texture;
         sf::Sprite sprite;
 
+        std::size_t frame_number;
+        bool looped;
+        bool paused;
+
+        double step_time;
+
+        void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
     };
 
 }
